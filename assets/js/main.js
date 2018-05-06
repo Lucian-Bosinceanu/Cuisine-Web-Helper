45215aaa77af1eb2953d6542dd5197d3f696b014
Serban Cobzac <serban.cobzac@gmail.com>
2018-05-06 16:39:19 +0300

    little bugs

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,9 @@ function toggleSidebar() {
     var sidebar = document.getElementById("sidebar");
 
     if (sidebar.className.indexOf("visible") >= 0)
-            sidebar.classList.remove("visible");
-            else
-            sidebar.classList.add("visible");
+        sidebar.classList.remove("visible");
+    else
+        sidebar.classList.add("visible");
 }
 
 function toggleDropdown(id) {
