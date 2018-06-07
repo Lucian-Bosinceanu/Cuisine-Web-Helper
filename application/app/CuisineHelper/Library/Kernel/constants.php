6fdab93e8f0eb7e0eb27276f6c394bb11231bd7c
medvichi-stefan <medvichi.stefan@gmail.com>
2018-06-07 22:35:15 +0300

    Modified error view

--- application/app/CuisineHelper/Library/Kernel/constants.php
+++ application/app/CuisineHelper/Library/Kernel/constants.php
@@ -1,4 +1,4 @@
 <?php
 define( 'ROOTPATH', $_SERVER["DOCUMENT_ROOT"] . "/../" );
-define( 'DEFAULT_VIEW_CACHE_PATH', ROOTPATH . "app/CuisineHelper/Library/Http/View/Views/cache" );
+define( 'DEFAULT_VIEW_CACHE_PATH', ROOTPATH . "storage/app/view/cache" );
 define( 'DEFAULT_VIEW_PATH', ROOTPATH . "app/CuisineHelper/Http/Views" );
