c55095fc0108e39ce2f8175565875fcf71f746d8
medvichi-stefan <medvichi.stefan@gmail.com>
2018-06-09 00:06:03 +0300

    - Installed Paris library - Changed routes file location - Added constants - Added assets folder

---
+++ application/public/assets/js/main.js
@@ -0,0 +1,19 @@
+function toggleSidebar() {
+
+    var sidebar = document.getElementById("sidebar");
+
+    if (sidebar.className.indexOf("visible") >= 0)
+            sidebar.classList.remove("visible");
+            else
+            sidebar.classList.add("visible");
+}
+
+function toggleDropdown(id) {
+
+    var element = document.getElementById(id);
+
+    if (element.className.indexOf("drop") >= 0)
+        element.classList.remove("drop");
+    else
+        element.classList.add("drop");
+}
