7e8aa05715cf119d972d8af787555aff56742c3e
Lucian-Bosinceanu <lucian.a.bosinceanu@gmail.com>
2018-06-12 08:10:17 +0300

    Fixed exports

--- application/public/assets/js/main.js
+++ application/public/assets/js/main.js
@@ -10,7 +10,7 @@ $(document).ready(function() {
         }
     });
 
-    $(".rss-button").on("click", function() {
+    $(".rss-button").on("click", function(e) {
         e.preventDefault();
         var location = window.location.href;
         window.open(location.substr(0, location.length - 1) + window.exportRssUrl, '_blank');
